22e5dd4f3bd60a0f1c4fcba974875086c77704e4
Gustavo <122051990+goosedlfdons@users.noreply.github.com>
2023-04-26 09:21:31 -0700

    Update HUD.java

--- HUD.java
+++ HUD.java
@@ -2,17 +2,31 @@ import java.awt.Color;
 import java.awt.Graphics;
 public class HUD {
 public static int Health = 100;
+public static int TowerHealth = 100;
 
 public void tick(){
 Health--;
+TowerHealth --;
 Health = Game.clamp(Health, 0, 100);
+TowerHealth = Game.clamp(TowerHealth, 0, 500);
+
 }
     public void render(Graphics g) {
-        g.setColor(Color.GRAY);
+        g.setColor(Color.red);
         g.fillRect(15, 15,200, 32);
-        g.setColor(Color.GREEN);
+        g.setColor(Color.green);
         g.fillRect(15, 15, Health * 2, 32);
-        g.setColor(Color.white);
+        g.setColor(Color.BLACK);
         g.drawRect(15, 15,200, 32);
     }
+
+    public void renderTower(Graphics g) {
+        g.setColor(Color.red);
+        g.fillRect(15, 80,200, 32);
+        g.setColor(Color.BLUE);
+        g.fillRect(15, 80, TowerHealth * 2, 32);
+        g.setColor(Color.black);
+        g.drawRect(15, 80,200, 32);
+    }
+
 }
