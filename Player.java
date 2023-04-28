5c68157bd555d079bbc8199d94a65cbdd4c627e1
Gustavo <122051990+goosedlfdons@users.noreply.github.com>
2023-04-28 14:15:36 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -5,20 +5,44 @@ import java.util.Random;
 public class Player extends GameObject {
 
     Random r = new Random();
-    public Player(int x, int y, ID id){
+    Handler handler;
+    public Player(int x, int y, ID id, Handler handler){
         super(x, y, id);
+        this.handler = handler;
 
        //velX = 1;
        //velX = r.nextInt(5) + 1;
        //velY = r.nextInt(5);
     }
 
+    public Rectangle getBounds(){
+        return new Rectangle(x, y, 32, 32);
+    }
+
     public void tick() {
         x += velX;
         y += velY;
+        if(y <= 0 || y >= Game.HEIGHT - 60) velY *= -1;
+        if(x <= 0 || x >= Game.WIDTH - 40) velX *= -1;
+
+        collision();
     }
 
+    private void collision(){
+        for(int i = 0; i < handler.object.size(); i++){
+            GameObject tempObject = handler.object.get(i);
+
+            if(tempObject.getId() == ID.Enemy){
+                //collision stuff
+                if(getBounds().intersects(tempObject.getBounds())){
+                    HUD.Health -= 2;
+                }
+            }
+        }
+    }
     public void render(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
         g.setColor(Color.BLUE);
         g.fillRect(x, y,32, 32);
     }
